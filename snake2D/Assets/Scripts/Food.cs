94a44df4c64dc25e994193dc427d9466a81c573b
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2022-02-07 17:48:50 +0530

    1st commit

---
+++ snake2D/Assets/Scripts/Food.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Head")
+        {
+            Destroy(gameObject);
+        }
+    }*/
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Head")
+        {
+            //Destroy(gameObject);
+            MoveFood();
+        }
+    }
+
+    public void MoveFood()
+    {
+
+        transform.position = new Vector2(-2, 3);
+    }
+
+
+
+}
---
+++ snake2D/Assets/Scripts/Head.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class Head : MonoBehaviour
+{
+
+    public float speed = 3f;
+    
+    public Vector2 direction; //= Vector2.right;
+
+    public GameObject tail;
+
+    public List<Transform> tailPositions;
+    public SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        ChangeDirection();
+    }
+
+    public void Move()
+    {
+        Vector2 lastPos = transform.position;
+
+
+
+        
+        transform.Translate(direction * speed * Time.deltaTime);
+
+
+        if (tailPositions.Count >= 1)
+        {
+            tailPositions.Last().position = lastPos;
+            tailPositions.Insert(0, tailPositions.Last());
+            tailPositions.RemoveAt(tailPositions.Count - 1);
+        }
+        
+
+        
+    }
+
+    public void ChangeDirection()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
+            sr.transform.eulerAngles = new Vector3(0, 0, -270);
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+
+            sr.transform.eulerAngles = new Vector3(0, 0, -90);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+
+            sr.transform.eulerAngles = new Vector3(0, 0, -180);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+
+            sr.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Vector2 spawnPos = new Vector2(10, 10);
+        if (collision.tag == "Food")
+        {
+            GameObject newTail = Instantiate(tail, spawnPos, Quaternion.identity) as GameObject;
+            newTail.transform.parent = GameObject.Find("Tail Holder").transform;
+            tailPositions.Add(newTail.transform);
+        }
+    }
+}
